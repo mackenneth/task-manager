f32ba5d57d980c6e98cfa7596394380202376a01
Maksim <mackenneth@yandex.ru>
2023-01-14 22:41:45 +0300

    feat create task list page header

---
+++ components/tasks/TaskListPageHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2 class="text-2xl">
+      Список задач
+    </h2>
+    <p class="font-light text-gray-600">
+      Успевай больше и ничего не забывай.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'TaskListPageHeader'
+}
+</script>
