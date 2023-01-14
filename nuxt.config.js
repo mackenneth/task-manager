c15380feb84b8ec416f655bd80e5ecf5314ba2a1
Maksim <mackenneth@yandex.ru>
2023-01-14 19:15:28 +0300

    init

---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+export default {
+  head: {
+    title: 'task-manager',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  css: [],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [
+    '@nuxt/typescript-build',
+    '@nuxt/postcss8',
+    '@pinia/nuxt',
+    '@nuxtjs/composition-api/module'
+  ],
+
+  modules: [
+    '@nuxtjs/axios',
+    '@pinia/nuxt'
+  ],
+
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {}
+      }
+    }
+  }
+}
