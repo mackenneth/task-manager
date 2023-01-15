3299ce51221ede8077b8aac929c9b69763ed2820
Maksim <mackenneth@yandex.ru>
2023-01-15 16:06:37 +0300

    feat create custom button

---
+++ components/base/CustomButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <button
+    class="
+      inline-block
+      px-6
+      py-2.5
+      bg-blue-600
+      text-white
+      font-medium
+      text-xs
+      leading-tight
+      uppercase
+      rounded
+      shadow-md
+      hover:bg-blue-700
+      hover:shadow-lg
+      focus:bg-blue-700
+      focus:shadow-lg
+      focus:outline-none
+      focus:ring-0
+      active:bg-blue-800
+      active:shadow-lg
+      transition
+      duration-150
+      ease-in-out
+  "
+    :type="type"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'CustomButton',
+  components: {},
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    }
+  }
+}
+</script>
