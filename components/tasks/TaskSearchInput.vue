a8d847ca21fce16721c4ffde941416a8467aa9d0
Maksim <mackenneth@yandex.ru>
2023-01-17 02:26:56 +0300

    feat create SearchInput

---
+++ components/tasks/TaskSearchInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <custom-input
+    v-model="searchQuery"
+    placeholder="Поиск..."
+  />
+</template>
+
+<script lang="ts">
+import { computed, WritableComputedRef } from 'vue'
+import CustomInput from '~/components/base/CustomInput.vue'
+import { useTasksStore } from '~/store/tasks'
+
+export default {
+  name: 'TaskSearchInput',
+  components: {
+    CustomInput
+  },
+  props: {},
+  setup () {
+    const store = useTasksStore()
+
+    const searchQuery: WritableComputedRef<string> = computed({
+      get: () => store.searchQuery,
+      set: (value) => {
+        store.updateSearchQuery(value)
+      }
+    })
+    return {
+      searchQuery
+    }
+  }
+}
+</script>
