78a3413396f362792e696f582cf37b562835dadb
Maksim <mackenneth@yandex.ru>
2023-01-17 11:17:46 +0300

    feat add terms of use

---
+++ pages/terms-of-use.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h2 class="text-2xl mb-5">
+      Правила использования
+    </h2>
+    <p class="mb-4">
+      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis possimus quos repellendus! Adipisci aut consequatur doloremque dolorum, illum laudantium libero necessitatibus nostrum quaerat quidem quis reprehenderit similique vero voluptatem voluptatibus.</span><span>Alias aut debitis dignissimos doloremque expedita natus necessitatibus nulla odio qui ut. Cum debitis doloribus et, explicabo illum magni nemo non similique sint tempora, totam ullam unde velit veritatis voluptas.</span><span>A consequatur corporis culpa, dolor dolore doloremque ducimus earum et ex, explicabo facere harum id ipsam itaque laboriosam maxime mollitia nihil nisi officia quia reiciendis sed tempora totam vero vitae!</span><span>Ab aut beatae, dolor esse laboriosam nam nulla sint sit suscipit. Ad aut incidunt iure, neque nihil placeat quia quibusdam quis quod saepe, sequi similique sint temporibus totam veniam vero.</span><span>Ab assumenda autem commodi debitis dolorem ducimus eligendi et expedita id impedit in iusto modi molestiae nemo nihil officia officiis placeat porro provident quo rem sed soluta, sunt, totam voluptatum?</span>
+    </p>
+    <p class="mb-4">
+      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis possimus quos repellendus! Adipisci aut consequatur doloremque dolorum, illum laudantium libero necessitatibus nostrum quaerat quidem quis reprehenderit similique vero voluptatem voluptatibus.</span><span>Alias aut debitis dignissimos doloremque expedita natus necessitatibus nulla odio qui ut. Cum debitis doloribus et, explicabo illum magni nemo non similique sint tempora, totam ullam unde velit veritatis voluptas.</span><span>A consequatur corporis culpa, dolor dolore doloremque ducimus earum et ex, explicabo facere harum id ipsam itaque laboriosam maxime mollitia nihil nisi officia quia reiciendis sed tempora totam vero vitae!</span><span>Ab aut beatae, dolor esse laboriosam nam nulla sint sit suscipit. Ad aut incidunt iure, neque nihil placeat quia quibusdam quis quod saepe, sequi similique sint temporibus totam veniam vero.</span><span>Ab assumenda autem commodi debitis dolorem ducimus eligendi et expedita id impedit in iusto modi molestiae nemo nihil officia officiis placeat porro provident quo rem sed soluta, sunt, totam voluptatum?</span>
+    </p>
+    <p class="mb-4">
+      <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis possimus quos repellendus! Adipisci aut consequatur doloremque dolorum, illum laudantium libero necessitatibus nostrum quaerat quidem quis reprehenderit similique vero voluptatem voluptatibus.</span><span>Alias aut debitis dignissimos doloremque expedita natus necessitatibus nulla odio qui ut. Cum debitis doloribus et, explicabo illum magni nemo non similique sint tempora, totam ullam unde velit veritatis voluptas.</span><span>A consequatur corporis culpa, dolor dolore doloremque ducimus earum et ex, explicabo facere harum id ipsam itaque laboriosam maxime mollitia nihil nisi officia quia reiciendis sed tempora totam vero vitae!</span><span>Ab aut beatae, dolor esse laboriosam nam nulla sint sit suscipit. Ad aut incidunt iure, neque nihil placeat quia quibusdam quis quod saepe, sequi similique sint temporibus totam veniam vero.</span><span>Ab assumenda autem commodi debitis dolorem ducimus eligendi et expedita id impedit in iusto modi molestiae nemo nihil officia officiis placeat porro provident quo rem sed soluta, sunt, totam voluptatum?</span>
+    </p>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'terms-of-use'
+}
+</script>
