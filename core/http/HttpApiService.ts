785ee6bd8697611197f28a71db4d184559061681
Maksim <mackenneth@yandex.ru>
2023-01-17 03:02:02 +0300

    feat create HTTP API service

---
+++ core/http/HttpApiService.ts
@@ -0,0 +1,26 @@
+import { AxiosInstance, AxiosPromise } from 'axios'
+import { TTask } from '~/core/types/tasks'
+
+export default class HttpApiService {
+  private axiosInstance: AxiosInstance = {} as AxiosInstance
+
+  private configs = {
+    tasks: '/todos'
+  }
+
+  constructor (axios: AxiosInstance) {
+    this.axiosInstance = axios
+  }
+
+  public addNewTask (task: TTask): AxiosPromise {
+    return this.axiosInstance.post(`${this.configs.tasks}`, task)
+  }
+
+  public getTasksByUserId (userId: number): AxiosPromise {
+    return this.axiosInstance.get(`${this.configs.tasks}?userId=${userId}`)
+  }
+
+  public editTaskById (task: TTask): AxiosPromise {
+    return this.axiosInstance.put(`${this.configs.tasks}/${task.id}`, task)
+  }
+}
