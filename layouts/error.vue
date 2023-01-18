e8304d877725a52e9b898b92adbd3519e5fe9c14
Maksim <mackenneth@yandex.ru>
2023-01-18 23:25:40 +0300

    feat add error page

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404" class="text-3xl text-center font-semi-bold mb-2">
+      Страница не найдена
+    </h1>
+    <img src="/img/error-404.png" class="w-full max-w-[650px] mb-4 mx-auto" alt="Логотип">
+    <NuxtLink to="/" class="block text-center mx-auto hover:text-blue-600 underline">
+      На главную
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blog',
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
